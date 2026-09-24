b83173f329b239fb313affca766174c344b310bb
Jakub Przybytek <jakub.przybytek@gmail.com>
2026-09-24 23:19:37 +0200

    DisplayController initial commit

---
+++ firmware/DisplayController/Core/Inc/main.h
@@ -0,0 +1,103 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32g0xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define LED1_Pin GPIO_PIN_13
+#define LED1_GPIO_Port GPIOC
+#define ADDR_0_Pin GPIO_PIN_10
+#define ADDR_0_GPIO_Port GPIOB
+#define ADDR_1_Pin GPIO_PIN_11
+#define ADDR_1_GPIO_Port GPIOB
+#define SWITCH_1_Pin GPIO_PIN_12
+#define SWITCH_1_GPIO_Port GPIOB
+#define SWITCH_2_Pin GPIO_PIN_13
+#define SWITCH_2_GPIO_Port GPIOB
+#define ADDR_2_Pin GPIO_PIN_14
+#define ADDR_2_GPIO_Port GPIOB
+#define DISPLAY_2_Pin GPIO_PIN_15
+#define DISPLAY_2_GPIO_Port GPIOA
+#define DISPLAY_5_Pin GPIO_PIN_0
+#define DISPLAY_5_GPIO_Port GPIOD
+#define DISPLAY_3_Pin GPIO_PIN_1
+#define DISPLAY_3_GPIO_Port GPIOD
+#define DISPLAY_4_Pin GPIO_PIN_2
+#define DISPLAY_4_GPIO_Port GPIOD
+#define DISPLAY_1_Pin GPIO_PIN_3
+#define DISPLAY_1_GPIO_Port GPIOD
+#define SCT_CLK_Pin GPIO_PIN_3
+#define SCT_CLK_GPIO_Port GPIOB
+#define SCT_SDI_Pin GPIO_PIN_5
+#define SCT_SDI_GPIO_Port GPIOB
+#define SCT_LA_Pin GPIO_PIN_6
+#define SCT_LA_GPIO_Port GPIOB
+#define SCT_EN_Pin GPIO_PIN_7
+#define SCT_EN_GPIO_Port GPIOB
+#define LOW_POWER_ENABLE_FLOAT_Pin GPIO_PIN_8
+#define LOW_POWER_ENABLE_FLOAT_GPIO_Port GPIOB
+#define LED2_Pin GPIO_PIN_9
+#define LED2_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
